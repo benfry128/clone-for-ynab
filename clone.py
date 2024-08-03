b047d4c307663bbc24c1ff6e3071ea2c9582c368
benfry128 <benfry128@gmail.com>
2024-08-02 22:53:26 -0400

    Trying to get budgets

--- clone.py
+++ clone.py
@@ -1,10 +1,7 @@
 import os
 from dotenv import load_dotenv
 import ynab_api
-from pprint import pprint
-from ynab_api.api import accounts_api
-from ynab_api.model.save_account_wrapper import SaveAccountWrapper
-from ynab_api.model.save_account import SaveAccount
+from ynab_api.api import budgets_api
 
 load_dotenv()
 YNAB_API_KEY = os.getenv('YNAB_API_KEY')
@@ -19,20 +16,11 @@ configuration.api_key_prefix['bearer'] = 'Bearer'
 
 # Enter a context with an instance of the API client
 with ynab_api.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = accounts_api.AccountsApi(api_client)
-    budget_id = "budget_id_example"  # str, none_type | The id of the budget (\"last-used\" can be used to specify the last used budget and \"default\" can be used if default budget selection is enabled (see: https://api.youneedabudget.com/#oauth-default-budget)
-    data = SaveAccountWrapper(
-        account=SaveAccount(
-            name="name_example",
-            type="checking",
-            balance=1,
-        ),
-    )  # SaveAccountWrapper | The account to create.
 
-    try:
-        # Create a new account
-        api_response = api_instance.create_account(budget_id, data)
-        pprint(api_response)
-    except ynab_api.ApiException as e:
-        print("Exception when calling AccountsApi->create_account: %s\n" % e)
+    api_instance = budgets_api.BudgetsApi(api_client)
+
+    budget_id = "budget_id_example"
+    include_accounts = True
+
+    api_response = api_instance.get_budgets(include_accounts=include_accounts)
+    print(api_response)
