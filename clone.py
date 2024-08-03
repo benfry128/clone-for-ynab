50cf2ac49e5997199669f8f5c0ada211f447921f
benfry128 <benfry128@gmail.com>
2024-08-03 19:34:56 -0400

    Switched from using ynab-api to just using requests to ping the ynab api directly

--- clone.py
+++ clone.py
@@ -1,26 +1,10 @@
 import os
 from dotenv import load_dotenv
-import ynab_api
-from ynab_api.api import budgets_api
+import requests
+from pprint import pprint
 
 load_dotenv()
 YNAB_API_KEY = os.getenv('YNAB_API_KEY')
 
-configuration = ynab_api.Configuration(
-    host="https://api.youneedabudget.com/v1"
-)
-
-configuration.api_key['bearer'] = YNAB_API_KEY
-configuration.api_key_prefix['bearer'] = 'Bearer'
-
-
-# Enter a context with an instance of the API client
-with ynab_api.ApiClient(configuration) as api_client:
-
-    api_instance = budgets_api.BudgetsApi(api_client)
-
-    budget_id = "budget_id_example"
-    include_accounts = True
-
-    api_response = api_instance.get_budgets(include_accounts=include_accounts)
-    print(api_response)
+r = requests.get('https://api.ynab.com/v1/budgets', headers={'Authorization': f'Bearer {YNAB_API_KEY}'})
+pprint(r.json())
