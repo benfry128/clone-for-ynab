6c98ae48e767e75befd0f7f164a7b0876090a1cc
benfry128 <benfry128@gmail.com>
2024-08-03 20:47:58 -0400

    Add accounts to ynab budget (will probably remove since fresh start does this but wanted to save this in case it's needed)

--- clone.py
+++ clone.py
@@ -5,6 +5,37 @@ from pprint import pprint
 
 load_dotenv()
 YNAB_API_KEY = os.getenv('YNAB_API_KEY')
+YNAB_URL = 'https://api.ynab.com/v1'
+s = requests.Session()
+s.headers.update({'Authorization': f'Bearer {YNAB_API_KEY}'})
 
-r = requests.get('https://api.ynab.com/v1/budgets', headers={'Authorization': f'Bearer {YNAB_API_KEY}'})
-pprint(r.json())
+old_id = '277f14e0-7fa0-4761-b14c-fbaa345e4175'
+new_id = '27573c3a-9388-4e91-8098-4a786d13bcab'
+
+budget = s.get(f'{YNAB_URL}/budgets/{old_id}').json()['data']['budget']
+new_budget = s.get(f'{YNAB_URL}/budgets/{new_id}').json()['data']['budget']
+
+
+# accounts = budget['accounts']
+
+# for account in accounts:
+#     acc = {
+#         'account': {
+#             'name': account['name'],
+#             'type': account['type'],
+#             'balance': 0
+#         }
+#     }
+#     s.post(f'{YNAB_URL}/budgets/{new_id}/accounts', json=acc)
+
+
+
+# first month, last month
+# payees
+# category groups
+# categories
+# months (budget info I think)
+# transactions
+# sub-transactions
+# scheduled transactions (ignore?)
+
